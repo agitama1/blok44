2e1c4d544d6e9af42bebded2ad61f4b20889bc17
agitama1 <93070268+agitama1@users.noreply.github.com>
2022-11-25 19:23:29 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "blok44.py",
+    version = "0.1",
+    py_modules = ['blok44'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
